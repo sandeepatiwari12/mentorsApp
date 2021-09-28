82615496ca873568eef1e95c123a0aeb0cf8efde
Sandeep Tiwari <33114476+sandeepatiwari12@users.noreply.github.com>
2021-09-28 13:48:28 +0530

    Delete run.sh

--- commands/run.sh
+++
@@ -1,8 +0,0 @@
-export SECRETTOKEN=mysecrettoken
-export MONGOURI="mongodb+srv://admin:admin@mentordb-yx8mq.mongodb.net/mentorDB?retryWrites=true&w=majority"
-export HOST=localhost
-export PORT=5000
-
-cd ../
-
-nodemon server
